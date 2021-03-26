c72535a1b843a6901e29d437c59b73a0b9db02c5
Jaro <12481969+WpSpeedDoctor@users.noreply.github.com>
2021-03-26 11:20:06 +0000

    Update admin-menu.php

--- admin-menu.php
+++ admin-menu.php
@@ -4,6 +4,13 @@ if ( ! defined( 'WPINC' ) ) {
   die;
 }
 
+if ( ! function_exists( 'is_inside_the_theme' ) ){
+	function is_inside_the_theme() {
+
+	return is_int( strpos(__DIR__, basename( get_theme_file_path() ) ));
+	}
+}
+
 if ( ! function_exists( 'register_bccss_plugin_settings' )){
 	function register_bccss_plugin_settings( $modules ) {
 		
@@ -17,13 +24,19 @@ if ( ! function_exists( 'register_bccss_plugin_settings' )){
 
 if ( ! function_exists( 'custom_menu' )){
 	function custom_menu() {
-		add_submenu_page('options-general.php', 
+
+		$admin_submenu_location = ( is_inside_the_theme()  ? 'themes.php': 'options-general.php' );
+
+		add_submenu_page( 
+
+			$admin_submenu_location, 
 			'Better Custom CSS', 
 			'Better Custom CSS', 
 			'administrator', 
 			'better_custom_css', 
 			'bc_css_admin_page', 
 			'dashicons-layout' 
+
 		);
 	}
 	add_action('admin_menu', 'custom_menu');
@@ -56,6 +69,21 @@ if ( ! function_exists( 'bc_css_admin_page' )){
 					<br>
 					<?php submit_button('Save settings'); ?>
 				</form>
+
+				<?php
+				if (!is_inside_the_theme()) {
+					
+					echo '<p style="max-width:600px">'._('You\'re running Better Custom CSS as a plugin. To avoid accidental deactivation, you can run Better Custom CSS code directly from the child theme. Just move plugin folder to the child theme folder and add to functions.php in the child theme:') . '</p>';
+					
+					echo '<p style="max-width: 600px;background-color: #ddd;width: fit-content;padding: 0 10px 3px;font-weight: 500;">require_once ( trailingslashit( get_theme_file_path() ) . \''.basename(__DIR__).'/better-custom-css.php\'); </p>';
+					
+					echo '<p style="max-width:600px">'._('After you do that, this plugin\'s settings menu will be automatically moved to theme menu "Appearance".') . '</p>';
+					
+					
+					
+				}
+				?>
+				
 			</div>
 		</div>
 		<?php
